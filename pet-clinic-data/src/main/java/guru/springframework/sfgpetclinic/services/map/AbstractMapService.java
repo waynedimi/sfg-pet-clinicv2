096ee2794299872feabe29154c4bd1051beb06d0
PapaFappy <73116483+waynedimi@users.noreply.github.com>
2022-08-17 10:06:55 +0200

    Updated last version

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,10 +1,11 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-
 public abstract class AbstractMapService<T ,ID> {
     protected Map<ID,T> map = new HashMap<>();
 
