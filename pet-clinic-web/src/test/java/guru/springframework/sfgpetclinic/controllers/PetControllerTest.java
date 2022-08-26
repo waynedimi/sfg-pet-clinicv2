b6f6c37631a089ba77da77b49791ba3b2f2605fe
PapaFappy <73116483+waynedimi@users.noreply.github.com>
2022-08-26 11:03:01 +0200

    Edited the PetType.java to have @builder manually not automated

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/PetControllerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void populatePetTypes() {
+    }
+
+    @Test
+    void findOwner() {
+    }
+
+    @Test
+    void initOwnerBinder() {
+    }
+}
